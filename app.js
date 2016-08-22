6f8b46e3d9f1b2e2372c6b1796412c1dcc4d9712
U-hp\admin <admin@hp.(none)>
2016-08-23 01:12:36 +0800

    abcreact

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,6 +10,26 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+console.log("start...");
+var cjs=fs.readFile('package.json', 'utf8', function (err, txt) {
+  console.log("step 1...");
+  if (err) {
+    throw err;
+  }
+  console.log("step 2...");
+  var djs=fs.readFile('app.js', 'utf8', function (err, content) {
+    console.log("step 3...");
+    if (err) {
+      throw err;
+    }
+    console.log("step 4...");
+    console.log(content);
+  });
+  console.log("step 5...");
+});
+
+console.log("over...");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
